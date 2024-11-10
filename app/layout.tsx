2d65efe63bf0265c1415534070eb6c2610358030
mkettel <mattkettelkamp@gmail.com>
2024-11-10 14:54:49 -0500

    added the view transitions for smoother transitions between pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
+import { ViewTransitions } from 'next-view-transitions'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,12 +31,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${montserrat.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang="en">
+        <body
+          className={`${montserrat.className} antialiased`}
+          >
+          {children}
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
